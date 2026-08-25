1d1d763b6b6ca29db36e8e80f9015418d280596f
ashenone123456 <yixingzhang0922@163.com>
2026-08-26 00:27:42 +0800

    Bump version code and version name

--- smartautoclicker/build.gradle.kts
+++ smartautoclicker/build.gradle.kts
@@ -57,8 +57,8 @@ android {
     defaultConfig {
         applicationId = getExtraActualApplicationId()
 
-        versionCode = 96
-        versionName = "4.0.0-quick-safe.1"
+        versionCode = 97
+        versionName = "4.0.0-quick-safe.2"
     }
 
     if (project.isBuildForVariant(KlickrFlavour.F_DROID, KlickrBuildType.DEBUG)) {
