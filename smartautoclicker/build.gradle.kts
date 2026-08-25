63bcc64f2955ff34e0e6bdfe492f15b2de5e7219
ashenone123456 <yixingzhang0922@163.com>
2026-08-25 18:20:23 +0800

    Version quick-safe debug build

--- smartautoclicker/build.gradle.kts
+++ smartautoclicker/build.gradle.kts
@@ -57,8 +57,8 @@ android {
     defaultConfig {
         applicationId = getExtraActualApplicationId()
 
-        versionCode = 95
-        versionName = "4.0.0"
+        versionCode = 96
+        versionName = "4.0.0-quick-safe.1"
     }
 
     if (project.isBuildForVariant(KlickrFlavour.F_DROID, KlickrBuildType.DEBUG)) {
